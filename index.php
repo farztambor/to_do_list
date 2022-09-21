2384d722f7c570ecfb5c3e4c5f5a6b88f0c4088d
farztambor <raffie.lanticse@gmail.com>
2022-09-21 15:55:33 +0800

    database connection and CSS Styles

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-<?php echo "Hello World "?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="main-section">
+        <div class="add-section">
+            <form action="" method="post">
+                <input type="text"
+                        name="title" 
+                        placeholder="This field is required"/>
+                <button type="submit">Add &nbsp; <span>&#43;</span></button>
+            </form>
+        </div>
+        <div class="show-todo-section">
+            <div class="todo-item">
+                <input type="checkbox" name="" id="">
+                <h2>This is checkbox</h2>
+                <br>
+                <small>created: 3/3/2020</small>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
